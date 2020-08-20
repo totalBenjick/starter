82f28b2cb34772ea965d71c081d352a4402eba63
totalBenjick <imoiseslezama@gmail.com>
2020-08-20 00:19:57 -0500

    Feat: improve dashboard.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Dashboard from "./components/Dashboard";
+import { Dashboard } from "./components/Dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
