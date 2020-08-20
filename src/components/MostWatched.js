5c4cce9fc0a25a4bb50f3127a59c1e3220d9177d
totalBenjick <imoiseslezama@gmail.com>
2020-08-20 00:29:58 -0500

    Feat: clean commented code

--- src/components/MostWatched.js
+++ src/components/MostWatched.js
@@ -21,13 +21,6 @@ export const MostWatched = () => {
       />
     ));
 
-  // const first = results?.[0];
-  // const urlToImage = first?.poster_path;
-  // const showName = first?.name;
-  // const averageVote = first?.vote_average;
-  // let img = `https://image.tmdb.org/t/p/w500/${urlToImage}`;
-  // console.log(loading, results, first, urlToImage, img);
-
   return (
     <>
       <span className=" lh-copy black pa1 tracked-tight sans-serif ma3 f2 mt5">{`Most Watched`}</span>
