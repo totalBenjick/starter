98d83f149332a600322e813f98418809a3e97bf2
totalBenjick <imoiseslezama@gmail.com>
2020-08-23 16:28:02 -0500

    Feat: add sort method

--- src/components/MostWatched.js
+++ src/components/MostWatched.js
@@ -3,13 +3,44 @@ import { useFetch } from "../hooks/useFetch";
 import { CardItem } from "./CardItem";
 
 export const MostWatched = () => {
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
   //Get most popular shows
   const getSeries = `https://api.themoviedb.org/3/tv/popular?api_key=1a09dcf42c6c621e5b547c2f53c489b3&language=en-US&page=${page}`;
 
   const { loading, data } = useFetch(getSeries);
 
   const results = data?.results || [];
+  console.log(results);
+
+  /**
+   * Function to sort alphabetically an array of objects by some specific key.
+   *
+   * @param {String} property Key of the object to sort.
+   */
+  const dynamicSort = (property) => {
+    var sortOrder = 1;
+
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+
+    return function (a, b) {
+      if (sortOrder == -1) {
+        return b[property].localeCompare(a[property]);
+      } else {
+        return a[property].localeCompare(b[property]);
+      }
+    };
+  };
+
+  const results_noModified = [...results];
+
+  const sortedAlphabetically = results_noModified.sort(
+    dynamicSort("original_name")
+  );
+
+  console.log(sortedAlphabetically);
 
   const MapResults = () =>
     results.map((e, i) => (
