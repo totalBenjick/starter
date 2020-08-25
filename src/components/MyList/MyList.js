7f913d0ccd9c16969b57c3a60d3ebfb6121ca738
totalBenjick <imoiseslezama@gmail.com>
2020-08-25 18:29:33 -0500

    Feat: section on components

--- src/components/MyList/MyList.js
+++ src/components/MyList/MyList.js
@@ -1,51 +1,38 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { favReducer } from "../../hooks/favoriteShow";
-import { useFetch } from "../../hooks/useFetch";
+import { ListItem } from "../ListItem/ListItem";
 
 export const MyList = () => {
-  // setState
-  const [state, setState] = useState({
-    data: null,
-    loading: true,
-    error: null,
-  });
-
   // Reducer to manage favorites and localStorage
   const init = () => {
     return JSON.parse(localStorage.getItem("favorites")) || [];
   };
-  const [favorites, dispatch] = useReducer(favReducer, [], init);
+  const [favorites] = useReducer(favReducer, [], init);
 
   useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favorites));
   }, [favorites]);
 
-  console.log(favorites);
-
-  const mapFavArray = () =>
-    favorites.map((e, i) => {
-      fetch(
-        `https://api.themoviedb.org/3/tv/${e}?api_key=1a09dcf42c6c621e5b547c2f53c489b3&language=en-US`
-      )
-        .then((resp) => resp.json())
-        .then((data) => {
-          setState({
-            loading: false,
-            error: null,
-            data,
-          });
-        });
-    });
+  const MapFavArray = () =>
+    favorites.map((e, i) => <ListItem id={e} key={i} />);
 
   useEffect(() => {
-    mapFavArray();
+    MapFavArray();
+    // eslint-disable-next-line
   }, [favorites]);
 
-  console.log(state);
   return (
     <div>
-      <div>{favorites}</div>
-
+      {favorites.length === 0 && (
+        <span className=" lh-copy black pa1 tracked-tight sans-serif ma3 f5 mt1 ml7">
+          <div
+            className={`ml5 mb3 `}
+          >{`Please add a show to your favorites  :)`}</div>
+        </span>
+      )}
+      <div className="tracked-tight sans-serif ml5 mt3 f3">
+        {"Liked Series"}
+      </div>
       <div className="pa4 helvetica">
         <div className="overflow-auto">
           <table className="f6 w-100 mw8 center" cellspacing="0">
@@ -55,21 +42,15 @@ export const MyList = () => {
                   Name
                 </th>
                 <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white dark-blue">
-                  Genre
+                  Average Vote
                 </th>
-                <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white red">
+                <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white dark-blue">
                   Delete
                 </th>
               </tr>
             </thead>
             <tbody className="lh-copy">
-              <tr>
-                <td className="pv3 pr3 bb b--black-20">
-                  Tv show name very long
-                </td>
-                <td className="pv3 pr3 bb b--black-20">Genre</td>
-                <td className="pv3 pr3 bb b--black-20">Icon to delete</td>
-              </tr>
+              <MapFavArray />
             </tbody>
           </table>
         </div>
