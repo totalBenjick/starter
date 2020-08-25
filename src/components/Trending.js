a92e436ae9f48989a13b69e896225e00b3540b12
totalBenjick <imoiseslezama@gmail.com>
2020-08-25 18:32:40 -0500

    Feat: added ViewContainer component

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { ViewContainer } from "./ViewContainer/ViewContainer";
 
 export const Trending = () => {
-  return <div>aqui los mas wachados</div>;
+  const urlProp =
+    "https://api.themoviedb.org/3/trending/tv/week?api_key=1a09dcf42c6c621e5b547c2f53c489b3";
+  const titleProp = "Trending";
+  return (
+    <>
+      <ViewContainer url={urlProp} title={titleProp} />
+    </>
+  );
 };
