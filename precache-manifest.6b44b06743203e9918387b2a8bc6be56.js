492919e825c9d118cfd718bc89beeb91c88374d4
totalBenjick <imoiseslezama@gmail.com>
2020-08-25 18:38:37 -0500

    Updates

---
+++ precache-manifest.6b44b06743203e9918387b2a8bc6be56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e073b188f5148db8e5b0fb63a1bbf9",
+    "url": "/starter/index.html"
+  },
+  {
+    "revision": "91689b2aa53deba40b26",
+    "url": "/starter/static/css/main.dbbc3e59.chunk.css"
+  },
+  {
+    "revision": "602a06dd890007708f81",
+    "url": "/starter/static/js/2.c4116994.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starter/static/js/2.c4116994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91689b2aa53deba40b26",
+    "url": "/starter/static/js/main.3e65c13a.chunk.js"
+  },
+  {
+    "revision": "0cc3a2da92580b336a11",
+    "url": "/starter/static/js/runtime-main.b11cf9ec.js"
+  }
+]);
